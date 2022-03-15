67f7ec440e9e6c5d44c7de2ea0126a4d23c5279c
Kevin Bond <kevinbond@gmail.com>
2022-03-15 16:06:38 -0400

    test redis sentinel

--- tests/RedisProvider.php
+++ tests/RedisProvider.php
@@ -24,11 +24,20 @@ trait RedisProvider
         if ($hosts = \getenv('REDIS_CLUSTER_HOSTS')) {
             yield [self::redisClusterDsn($hosts), \RedisCluster::class];
         }
+
+        if (\getenv('REDIS_SENTINEL_HOST') && \getenv('REDIS_SENTINEL_SERVICE')) {
+            yield [self::redisSentinelDsn(), \Redis::class];
+        }
     }
 
     public static function redisDsn(): string
     {
         return 'redis://'.\getenv('REDIS_HOST1');
+    }
+
+    public static function redisSentinelDsn(): string
+    {
+        return 'redis://'.\getenv('REDIS_SENTINEL_HOST').'?redis_sentinel='.\getenv('REDIS_SENTINEL_SERVICE');
     }
 
     public static function redisArrayDsn(): string
